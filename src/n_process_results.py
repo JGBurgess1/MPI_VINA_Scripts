7c32f2c6bf742e452f1de36bcd993929a8714bbd
JGBurgess1 <133377607+JGBurgess1@users.noreply.github.com>
2023-08-05 10:46:52 +0200

    Update n_process_results.py

--- src/n_process_results.py
+++ src/n_process_results.py
@@ -2,11 +2,11 @@ import os
 
 def process(directory):
     """Method grabs the results of the top scoring docked pose, and prints the result with its ZINC identifier and tranche identifier to the results file."""
-    for file in os.listdir(directory+"Output/"):
+    for file in os.listdir(directory+"/Output/"):
         if file[-5:] == "pdbqt":
             lines = []
 
-            with open(f'{directory}Output/{file}', "r") as new_file:
+            with open(f'{directory}/Output/{file}', "r") as new_file:
                 lines.append(new_file.readline())
                 lines.append(new_file.readline())
                 lines.append(new_file.readline())
@@ -21,12 +21,5 @@ def process(directory):
             Vina_score = lines[1][3]
 
             new_line_in_file = ", ".join([ZINC_name,filename,Vina_score])
-
-            with open(directory+"Output/results", "a") as results_file:
+            with open(directory+"/Output/results", "a") as results_file:
                 results_file.write(new_line_in_file + "\n")
-
-
-def main():
-    process("/mnt/lustre/users/jburgess1/ZINC_DB_Test/AA/AAML/")
-
-main()

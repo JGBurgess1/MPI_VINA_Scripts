aa5434552ac54b13b8f2fa1d731d4c41b7435cea
Jeremy Gareth Burgess <133377607+JGBurgess1@users.noreply.github.com>
2024-04-28 15:39:44 +0200

    ..

--- src/JobChecker.py
+++ src/JobChecker.py
@@ -4,8 +4,7 @@
 
 import subprocess
 
-result = subprocess.run(['qstat','large','|','grep','jburgess1'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-
+result = subprocess.run(['qstat','large'],stdout=subprocess.PIPE)
 output = result.stdout
 
 print(output)
